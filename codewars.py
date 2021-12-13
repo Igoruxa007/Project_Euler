f8358f9662359c30412b0d4202611e414b7539e4
Igoruxa007 <74563405+Igoruxa007@users.noreply.github.com>
2021-12-13 16:10:57 +0300

    Isogram from codewars

--- codewars.py
+++ codewars.py
@@ -89,4 +89,15 @@ def square_sum(numbers):
     return sum
 
 
-print(square_sum([1, 2]))
+#_____________________________________________________________ Isogram
+def is_isogram(string):
+    string = string.lower()
+    for i in range(len(string)):
+        for j in range(i+1, len(string)):
+            if string[i] == string[j]:
+                return False
+                break
+    return True
+
+
+print(is_isogram("Dermatoglyphics"))
